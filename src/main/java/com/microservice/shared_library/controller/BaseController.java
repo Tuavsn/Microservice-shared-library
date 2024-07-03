3e0856ee03d06bd630293eb612300e717f670991
Tuavsn <hoctuavsn@gmail.com>
2024-07-03 16:49:22 +0700

    config

--- src/main/java/com/microservice/shared_library/controller/BaseController.java
+++ src/main/java/com/microservice/shared_library/controller/BaseController.java
@@ -1,4 +1,57 @@
 package com.microservice.shared_library.controller;
 
-public class BaseController {
+import com.microservice.shared_library.dto.BaseDTO;
+import com.microservice.shared_library.model.BaseEntityAudit;
+import com.microservice.shared_library.service.BaseService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class BaseController <Model extends BaseEntityAudit, DTO extends BaseDTO, ID extends UUID> {
+    private final BaseService<Model, DTO, ID> service;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<DTO>> getAll() {
+        return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DTO>> getAll(
+            @ParameterObject Pageable pageable, @RequestParam(defaultValue = "") String search
+    ) {
+        return new ResponseEntity<>(service.findAll(pageable, search), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<DTO>> get(@PathVariable Set<ID> id) {
+        return new ResponseEntity<>(service.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<DTO> create(@Valid @RequestBody DTO dto) {
+        dto.setId(null);
+        return new ResponseEntity<>(service.save(dto), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DTO> update(@PathVariable ID id, @Valid @RequestBody DTO dto) {
+        dto.setId(id);
+        return new ResponseEntity<>(service.save(dto), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<DTO> delete(@PathVariable Set<ID> id) {
+        service.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
